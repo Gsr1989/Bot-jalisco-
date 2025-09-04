60337043782a49ced4ead87222f740a42338b74d
Gsr1989 <empresalorens@gmail.com>
2025-09-04 09:25:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ URL_CONSULTA_BASE = "https://serviciodigital-jaliscogobmx.onrender.com"
 
 # Coordenadas para el QR (en una posición que no interfiera con el contenido existente)
 coords_qr_dinamico = {
-    "x": 50,
-    "y": 600,
-    "ancho": 80,
-    "alto": 80
+    "x": 925,
+    "y": 640,
+    "ancho": 110,
+    "alto": 110
 }
 
 # ------------ SUPABASE ------------

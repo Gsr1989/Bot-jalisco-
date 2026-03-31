b55cd4bb7574c60a7ad7f001712838057b071902
Gsr1989 <empresalorens@gmail.com>
2026-03-31 13:23:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -505,7 +505,7 @@ def generar_pdf_unificado(datos: dict) -> str:
 
         incrementar_folio_representativo(fol_rep)
 
-        pg1.insert_text((935, 600), f"*{fol}*", fontsize=30, color=(0, 0, 0), fontname="Courier")
+        pg1.insert_text((935, 700), f"*{fol}*", fontsize=30, color=(0, 0, 0), fontname="Courier")
 
         pg1.insert_text((915, 775), "EXPEDICION: VENTANILLA DIGITAL", fontsize=12, color=(0, 0, 0), fontname="hebo")
 
